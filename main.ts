3715e0e389c90ff1b3fa84af35cdef56c751ebf8
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-05-27 02:57:49 +0500

    This is Object , Array and function. Assignment 2:Manipulating an Array: Rearranging Words
    This is Object , Array and function. Assignment 2:Manipulating an Array: Rearranging Words

---
+++ main.ts
@@ -0,0 +1,31 @@
+//Assignment 2:Manipulating an Array: Rearranging Words
+
+//Objective:
+//Rearrange an array using array methods to form the sentence "I am a student of GIAIC".
+//Steps:
+//1. Scrambled Array:
+//o Start with an array of elements in a scrambled order, like:
+//const scrambledArray = ["student", "of",true, 123, "am", "a", "GIAIC", "I"];
+
+//• Modify the Array:
+//• Use methods like split(), join(), push(), pop(), shift(), and unshift() to:
+//o Convert non-strings (booleans, numbers) to strings if needed.
+//o Split elements into character arrays (optional).
+//o Rearrange characters or elements in the desired order (modify original array or
+//create temporary arrays).
+//• Output the Result:
+//• Use join() to combine elements back into a single string: "I am a student of GIAIC".
+//• Output the Result:
+//• Use join() to combine elements back into a single string: "I am a student of GIAIC".
+
+let scrambledArray = ["I", "am", "a", "student", "of", "GIAIC"];  
+
+scrambledArray.splice(2, 2)
+scrambledArray.pop()
+scrambledArray.unshift("I")
+scrambledArray[1] = "am";
+scrambledArray[2] = "a";
+scrambledArray[3] = "student";
+scrambledArray[4] = "of";
+scrambledArray[5] = "GIAIC";
+console.log(scrambledArray.join(" "));
